e371f31621a46bd7151c6cec2f0c2c641a2a1584
Hudson Kennedy <hudsonhugo@bol.com.br>
2022-05-12 18:23:29 -0300

    update API

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,47 @@
 <template>
-    <div id="app">
-        <h1 class="text-center">Conversor de Moedas</h1>
-       <div class="linha">
-       <Conversor moeda-a="USD" moeda-b="BRL" /> 
-       <Conversor moeda-a="BRL" moeda-b="USD" /> 
-       </div>
-       <div class="linha">
-       <Conversor moeda-a="CAD" moeda-b="BRL" /> 
-       <Conversor moeda-a="BRL" moeda-b="CAD" /> 
-       </div>
-       <div class="linha">
-       <Conversor moeda-a="EUR" moeda-b="BRL" /> 
-       <Conversor moeda-a="BRL" moeda-b="EUR" /> 
-       </div>
+  <div id="app">  
+    <h1>Conversor de moedas</h1>
+    <div class="linha">
+      <conversor moeda-a="USD" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="USD"></conversor>
     </div>
+
+    <div class="linha">
+      <conversor moeda-a="CAD" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="CAD"></conversor>
+    </div>
+    
+    <div class="linha">
+      <conversor moeda-a="EUR" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="EUR"></conversor>
+    </div>
+  </div>
 </template>
 
 <script>
-import Conversor from "./components/Conversor.vue"
-
- export default {
-     name: "app",
-     components: {
-         Conversor
-     }
- };
+import Conversor from './components/Conversor.vue';
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
 </script>
 
-<style >
+<style>
 #app {
-    margin: auto;
-    max-width: 900px;
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    
-    color:#2c3e50;
-    margin-top: 60px;
+  margin: auto;
+  max-width: 900px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-.linha{
-    margin-top: 40px;
-    display: flex;
-    justify-content: space-around;
-}
-.text-center{
-    text-align: center;
+.linha {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,51 +1,48 @@
 <template>
     <div class="conversor">
-<h2>{{moedaA}} para {{moedaB}}</h2>
-<input type="text" v-model="moedaA_value" v-bind:Placeholder="moedaA">
-<input type="button" value="Converter" v-on:click="converter" >
-<h2></h2>
-    </div>
+        <h2>{{ moedaA }} Para {{ moedaB }}</h2>
+        <input type="text" :placeholder="moedaA" v-model="moedaA_value">
+        <input type="button" value="Converter" v-on:click="converter">
+        <h2>{{ moedaB_value }}</h2>
+    </div>    
 </template>
 
 <script>
- export default {
-     name: "conversor",
-     props: ["moedaA", "moedaB"],
-      data(){
-          return {
-              moedaA_value : "",
-              moedaB_value : 0
-          };
-      },
-      methods: {
-          converter(){
-              let de_para = this.moedaA + "_" + this.moedaB;
-
-              let url = "http://economia.awesomeapi.com.br/json/last/"+
-                de_para
-              
-
-              fetch(url)
-               .then(res=>{
-                   return res.json();
-                   })
-                        .then(json=>{
-                            let cotacao = json[de_para].val;
-                            this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(
-                                2
-                            );
-                        });
-          }
-      }
- };
+export default {
+    name: 'Conversor',
+    props: [
+        'moedaA',
+        'moedaB'
+    ],
+    data() {
+        return {
+            moedaA_value: '',
+            moedaB_value: 0
+        }
+    },
+    methods: {
+        converter() {
+            let de_para = this.moedaA + "_" + this.moedaB;
+            let url = "https://free.currconv.com/api/v7/convert?q=" + de_para + "&compact=ultra&apiKey=a6f98962807910b11324";
+        fetch(url)
+            .then(response => {
+                console.log(url)
+                return response.json();
+            })
+            .then(json => {
+                console.log(json[de_para])
+                let cotacao = json[de_para];
+                this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+            });
+        }
+    },
+}
 </script>
 
 <style scoped>
-
-.conversor{
+.conversor {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     padding: 20px;
     max-width: 300px;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0, 0.2);
-};
-
+}
 </style>
